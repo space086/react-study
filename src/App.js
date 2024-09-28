667f48df32c8b2563702640af4894884d7721bfe
space086 <space_086@naver.com>
2024-09-28 23:38:33 +0900

    🐟react-study /240928 - day08/ clean-up

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@
 import { useState, useEffect } from "react";
 import styles from "./App.module.css";
 
+function Hello() {
+  useEffect(function () {
+    console.log("hi :)");
+    return function () {
+      console.log("bye :(");
+    };
+  }, []);
+}
+
 function App() {
+  // useEffect
   const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState(""); 
+  const [keyword, setKeyword] = useState("");
+  // cleanup
+  const [showing, setShowing] = useState(false);
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
+  const clickBtn = () => setShowing((prev) => !prev)
   console.log("i run all the time");
   // const iRunOnlyOnce = () => {
   //   console.log("i run only once.");
@@ -33,12 +46,17 @@ function App() {
   useEffect(() => {
     console.log("i run when 'keyword & counter' changes.");
   }, [keyword, counter]);
+
   return (
     <div>
       <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."></input>
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>click me</button>
       {/* <Button text={"Continue"} /> */}
+      <hr></hr>
+      {/* cleanUp */}
+      {showing ? <Hello /> : null}
+      <button onClick={clickBtn}>{showing ? "hide" : "show"}</button>
     </div>
   );
 }
