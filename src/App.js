b418e90c95a474e52293eb13eff27da50b670a04
space086 <space_086@naver.com>
2024-09-28 17:45:55 +0900

    🐟react-study /240928 - day08/ useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import Button from "./Button";
+// import Button from "./Button";
+import { useState, useEffect } from "react";
 import styles from "./App.module.css";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  // const iRunOnlyOnce = () => {
+  //   console.log("i run only once.");
+  // }
+  // useEffect(iRunOnlyOnce, []);
+
+  useEffect(()=> {
+    console.log("CALL THE API...");
+  }, [])
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+      {/* <Button text={"Continue"} /> */}
     </div>
   );
 }
