62a0fd2dae63510245a889c33ab08e0836d33650
space086 <space_086@naver.com>
2024-09-28 18:07:25 +0900

    🐟react-study /240928 - day08/ deps

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import styles from "./App.module.css";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState(""); 
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
   // const iRunOnlyOnce = () => {
   //   console.log("i run only once.");
@@ -12,10 +14,28 @@ function App() {
   // useEffect(iRunOnlyOnce, []);
 
   useEffect(()=> {
-    console.log("CALL THE API...");
-  }, [])
+    console.log("i run only once");
+  }, []);
+  // useEffect(()=> {
+  //   if(keyword !== "" && keyword.length > 5) {
+  //     console.log("SEARCH FOR", keyword);
+  //   }
+  // }, [keyword]);
+//   useEffect(()=> {
+//     console.log("SEARCH FOR", keyword);
+// }, [keyword]);
+  useEffect(() => {
+    console.log("i run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("i run when 'keyword & counter' changes.");
+  }, [keyword, counter]);
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."></input>
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>click me</button>
       {/* <Button text={"Continue"} /> */}
