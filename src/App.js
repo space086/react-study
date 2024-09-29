835ae363066ecb2f48cd0e3844d57e1d68510c12
space086 <space_086@naver.com>
2024-09-29 15:59:09 +0900

    🐟react-study /240929 - day09/ 영화 소개사이트

--- src/App.js
+++ src/App.js
@@ -103,7 +103,6 @@ export default App;
 /*
 // 암호화폐 사이트 (09.29)
 
-*/
 
 import { useState, useEffect } from "react";
 
@@ -129,13 +128,64 @@ function App() {
         <select>
           {coins.map((coin) => (
             <option key={coin.id}>
-              {/* {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD */}
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
             </option>
           ))}
         </select>
       )}
   </div>
+  );
+}
+
+export default App;
+*/
+
+/*
+// 영화 소개 사이트 (09.29)
+
+*/
+
+import { useState, useEffect } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    console.log("json", json.data)
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <img src={movie.medium_cover_image} />
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            {movie.hasOwnProperty("genres")? 
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            : null}
+          </div>
+        ))}
+        </div>
+      )}
+    </div>
   );
 }
 
