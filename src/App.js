67c989023e7cb39fe6ce15357aba64e89782beb7
space086 <space_086@naver.com>
2024-09-30 22:47:36 +0900

    🐟react-study /240930 - day09/ 영화 소개사이트 - 라우터

--- src/App.js
+++ src/App.js
@@ -201,7 +201,10 @@ function App() {
         <Route path="/hello">
           <h1>Hello</h1>
         </Route>
-        <Route path="/movie">
+        <Route path="/abot-us">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie/:id">
           <Detail />
         </Route>
         <Route path="/">
