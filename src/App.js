ad8097bf0e40135480a2361b3d2ca5ebcc04d7e4
space086 <space_086@naver.com>
2024-10-13 23:54:08 +0900

    🐟react-study /241013 - day18/ class components and state

--- src/App.js
+++ src/App.js
@@ -218,6 +218,8 @@ function App() {
 }
 
  */
+
+/* jsx + props 
 import React from "react";
 import PropTypes from "prop-types";
 
@@ -289,5 +291,32 @@ function App() {
   );
 }
 
+
+export default App;
+*/
+
+import React from "react";
+import PropTypes from "prop-types";
+
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
+}
 
 export default App;
