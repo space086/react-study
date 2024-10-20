846e5fdaafc4dd5179dc19d2a252b2e47e3e5a60
space086 <space_086@naver.com>
2024-10-20 22:49:57 +0900

    🐟react-study /241020 - day24/ styling the movie

--- src/App.js
+++ src/App.js
@@ -298,8 +298,7 @@ export default App;
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
-
-
+import "./App.css";
 class App extends React.Component {
   state = {
     isLoading: true,
@@ -321,10 +320,14 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map(movie => (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map(movie => (
               <Movie
                 key={movie.id}
                 id={movie.id}
@@ -334,7 +337,9 @@ class App extends React.Component {
                 poster={movie.medium_cover_image}
               />
             ))}
-      </div>
+        </div>
+        )}
+      </section>
     );
   }
 }
