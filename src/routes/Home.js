66468e175cbb3db3bd9f96530b9e024746ba5578
space086 <space_086@naver.com>
2024-10-25 23:22:21 +0900

    🐟react-study /241025 - day29/ arrange

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,44 +1,52 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import axios from "axios";
 import Movie from "../components/Movie";
-import styles from "./Home.module.css";
+import "./Home.css";
 
-function Home() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
   };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-    <div className={styles.container}>
-      {loading ? (
-        <div className={styles.loader}>
-          <span>Loading...</span>
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+          <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
         </div>
-      ) : (
-        <div className={styles.movies}>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+        )}
+      </section>
+    );
+  }
 }
+
 export default Home;
