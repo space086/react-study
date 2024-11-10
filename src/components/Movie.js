546013e00288d09ee211a815388fbc98a219c9c4
space086 <space_086@naver.com>
2024-11-10 16:29:43 +0900

    🐟react-study /241109 - day35/ 영화 사이트 복습 - parameters

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,12 @@
 import PropTypes from "prop-types";
-
-function Movie({ coverImg, title, summary, genres }) {
+import { Link } from "react-router-dom";
+function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
@@ -16,6 +18,7 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 Movie.prototype = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
